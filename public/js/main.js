b1d7b230c8a822b5d8a819efaacdda3b452d4b55
aqeelaman <AA5226@live.mdx.ac.uk>
2024-04-25 19:25:51 +0400

    changed hospitals storage and map functionalities

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,31 @@
-var hospitals = [];
-var doctors = [];
-// Define global variables
+
+var hospitals;
 var map;
 var marker;
 var hospitalMarkersArray = []; // Array to store hospital markers
 var currentWindow = null;
-var myCoords = "25.204751, 55.270942";
+var myLatlng;
 
-init();
+init()
+function init() {
+    if (window.location.href === "http://localhost:3000/html/index.html") {
+        index();
+    }
+}
 
-async function init() {
+async function index() {
+
+    //if (sessionStorage.getItem(newUser) == null) {
     await getHospitals();
-    await getDoctors();
-    calculateInitialDistance();
+    //await getDoctors();
     await initMap();
-    //console.log(hospitals);
+    //sessionStorage.setItem("newUser", false);
+    //}
 };
 
 async function getHospitals() {
-
     try {
-        const response = await fetch('/tabibsouq/hospitals');
+        const response = await fetch('/getHospitals');
 
         if (response.ok) {
             hospitals = await response.json();
@@ -36,57 +41,6 @@ async function getHospitals() {
 
 }
 
-function calculateInitialDistance() {
-    for (let i = 0; i < hospitals.length; i++) {
-        hospitals[i].distance = calculateDistance(createLatLng(myCoords), createLatLng(hospitals[i].coordinates));
-    }
-    sortDoctors();
-}
-
-function createLatLng(str) {
-    const [latStr, lngStr] = str.split(',').map(parseFloat);
-    return {
-        lat: () => latStr,
-        lng: () => lngStr
-    };
-}
-
-async function getDoctors() {
-
-    try {
-        const response = await fetch('/tabibsouq/doctors');
-
-        if (response.ok) {
-            doctors = await response.json();
-            console.log(doctors);
-        }
-        else {
-            console.log("HTTP error: " + response.status)
-        }
-
-    } catch (error) {
-        console.error("Error fetching hospitals: " + error);
-    }
-
-}
-
-function sortDoctors() {
-    // Iterate over each hospital
-    hospitals.forEach(hospital => {
-        // Sort doctors array based on the distance to this hospital
-        doctors.sort((a, b) => {
-            const hospitalA = hospitals.find(h => h._id === a.dr_hospital[0]);
-            const hospitalB = hospitals.find(h => h._id === b.dr_hospital[0]);
-
-            // Check if hospitalA and hospitalB are valid objects
-            if (hospitalA && hospitalB) {
-                return hospitalA.distance - hospitalB.distance;
-            }
-        });
-    });
-    console.log(doctors);
-}
-
 function initMap() {
 
     document.getElementById('map-button').addEventListener('click', function () {
@@ -98,8 +52,16 @@ function initMap() {
         }
     });
 
+    if (sessionStorage.getItem("location") == null) {
+        var myLatlng = new google.maps.LatLng(25.204751, 55.270942);
+        sessionStorage.setItem("location", myLatlng);
+    }
+    else {
+        var coords = sessionStorage.getItem("location").replace(/[()]/g, '').split(',');
+        var myLatlng = new google.maps.LatLng(parseFloat(coords[0]), parseFloat(coords[1]));
+    }
+    //var myLatlng = sessionStorage.getItem("location") || new google.maps.LatLng(25.204751, 55.270942);
 
-    var myLatlng = new google.maps.LatLng(25.204751, 55.270942);
     map = new google.maps.Map(document.getElementById('map-container'), {
         center: myLatlng,
         zoom: 16,
@@ -130,24 +92,57 @@ function initMap() {
     google.maps.event.addListener(map, 'click', function (event) {
         var myLatlng = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
         marker.setPosition(myLatlng);
-        hospitalMarkers(); // Update hospital markers when map is clicked
+        sessionStorage.setItem("location", myLatlng);
+        updateHospitals(myLatlng)
+            .then(() => { hospitalMarkers(); })
+            .catch(error => { console.error('Error updating hospitals:', error); });
     });
 
     // Add dragend event listener to marker
     google.maps.event.addListener(marker, 'dragend', function (event) {
-        hospitalMarkers(); // Update hospital markers when marker is dragged
+        myLatlng = marker.getPosition();
+        sessionStorage.setItem("location", myLatlng);
+        updateHospitals(myLatlng)
+            .then(() => { hospitalMarkers(); })
+            .catch(error => { console.error('Error updating hospitals:', error); });
     });
 
     hospitalMarkers(); // Initial call to show hospital markers
 }
 
+async function updateHospitals(userCoords) {
+    coordinates = `${userCoords.lat()}, ${userCoords.lng()}`
+    try {
+        const response = await fetch('/updateHospitals', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ coordinates })
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.message);
+            await getHospitals();
+            await displayNearByDoctors();
+        } else {
+            console.error('Failed to update hospitals:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error updating hospitals:', error);
+    }
+}
+
 // Function to display user's position
 function showPosition(position) {
-    var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     marker.setPosition(myLatlng);
     map.setCenter(myLatlng);
     map.setZoom(17);
-    hospitalMarkers(); // Update hospital markers when user's position is obtained
+    sessionStorage.setItem("location", myLatlng);
+    updateHospitals(myLatlng)
+        .then(() => { hospitalMarkers(); })
+        .catch(error => { console.error('Error updating hospitals:', error); });
 }
 
 // Function to handle errors in geolocation
@@ -168,22 +163,12 @@ function showError(error) {
     }
 }
 
-// Function to calculate distance between two coordinates
-function calculateDistance(coord1, coord2) {
-    const R = 6371; // Radius of the Earth in kilometers
-    const dLat = (coord2.lat() - coord1.lat()) * Math.PI / 180; // Convert degrees to radians
-    const dLon = (coord2.lng() - coord1.lng()) * Math.PI / 180;
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(coord1.lat() * Math.PI / 180) * Math.cos(coord2.lat() * Math.PI / 180) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
-    return parseFloat(distance.toFixed(2));
-}
-
 // Function to display hospital markers
 function hospitalMarkers() {
+    if (!hospitals) {
+        return;
+    }
+
     // Clear existing markers
     clearHospitalMarkers();
 
@@ -208,7 +193,6 @@ function hospitalMarkers() {
 
         // Calculate distance from user marker (if available)
         if (marker) {
-            hospitals[i].distance = calculateDistance(marker.getPosition(), latLng);
             let contentString = '<div><strong>' + hospitals[i].location + '</strong><br><strong>' + hospitals[i].distance + ' km</strong> </div>';
             let infowindow = new google.maps.InfoWindow({
                 content: contentString
@@ -225,7 +209,6 @@ function hospitalMarkers() {
         // Store marker in array
         hospitalMarkersArray.push(hospitalMarker);
     }
-    sortDoctors();
 }
 
 // Function to clear existing hospital markers
@@ -235,4 +218,78 @@ function clearHospitalMarkers() {
     }
     hospitalMarkersArray = [];
     currentWindow = null;
+}
+
+
+function drSpeciality(dr_speciality) {
+    var str = dr_speciality[0];
+    for (let i = 1; i < dr_speciality.length; i++) {
+        str += ", "
+        str += dr_speciality[i];
+    }
+    return str;
+}
+
+function drHospital(dr_hospital) {
+    for (let i = 0; i < hospitals.length; i++) {
+        if (dr_hospital[0] == hospitals[i]._id) {
+            return hospitals[i];
+        }
+    }
+}
+
+function drDistance(dr_hospital) {
+    for (let i = 0; i < hospitals.length; i++) {
+        if (dr_hospital[0] == hospitals[i]._id) {
+            return hospitals[i].distance;
+        }
+    }
+}
+
+function drLanguages(dr_languages) {
+    var str = dr_languages[0];
+    for (let i = 1; i < dr_languages.length; i++) {
+        str += ", "
+        str += dr_languages[i];
+    }
+    return str;
+}
+
+function drExperience(dr_experience) {
+    var str = dr_experience[0];
+    for (let i = 1; i < dr_experience.length; i++) {
+        str += ", "
+        str += dr_experience[i];
+    }
+    return str;
+}
+
+function drEducation(dr_education) {
+    var str = dr_education[0];
+    for (let i = 1; i < dr_education.length; i++) {
+        str += ", "
+        str += dr_education[i];
+    }
+    return str;
+}
+
+function generateRatingStars(rating) {
+    var stars = '';
+    var fullStars = Math.floor(rating);
+    var halfStars = Math.ceil(rating - fullStars);
+    var emptyStars = 5 - fullStars - halfStars;
+
+    for (var i = 0; i < fullStars; i++) {
+        stars += '<span class="star">&#9733;</span>';
+    }
+
+    if (halfStars === 1) {
+        stars += '<span class="star">&#9734;&#9733;</span>'; // Half star
+    }
+
+    for (var j = 0; j < emptyStars; j++) {
+        stars += '<span class="star">&#9734;</span>';
+    }
+
+    return stars;
 }
